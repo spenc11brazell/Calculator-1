afab3930bac5b5bd01a81d13936ddfda995495ff
Spencer Brazell <sa.brazell11@gmail.com>
2022-02-19 15:19:12 -0700

    addition implementation working

--- calc.js
+++ calc.js
@@ -7,15 +7,46 @@ const reader = readline.createInterface({
 
 
 
-reader.question("What would you like to calculate?", function(input){
-	tokens = input.split(' ');
+// reader.question("What would you like to calculate?", function(input){
+// 	tokens = input.split(' ');
 	
+// 	mathSymbol = tokens[0];
+// 	num1 = Number(tokens[1]);
+// 	num2 = Number(tokens[2]);
+
+
+// 	// This line closes the connection to the command line interface.
+// 	reader.close()
+
+// });
+
+reader.question("What would you like to calculate?", function(input){
+		tokens = input.split('');
+
 	mathSymbol = tokens[0];
-	num1 = Number(tokens[1]);
-	num2 = Number(tokens[2]);
+		num1 = Number(tokens[1]);
+		num2 = Number(tokens[2]);
 
+		if (mathSymbol === "+"){
+				console.log(num1 + num2);
+		}
 
-	// This line closes the connection to the command line interface.
-	reader.close()
+		if (mathSymbol === "-"){
+				console.log(num1 - num2);
+		}
 
-});
+		if (mathSymbol === "*"){
+				console.log(num1 * num2);
+		}
+
+		if (mathSymbol === "/"){
+				console.log(num1 / num2);
+		}
+
+		if (mathSymbol === "sqrt"){
+				console.log(Math.sqrt(num1));
+		}
+
+		reader.close()
+
+	});
